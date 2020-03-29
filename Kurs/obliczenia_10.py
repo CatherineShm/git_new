7549466107e4ea3a0a84e1cc364e53562be151ab
CatherineShm <katerynashmaliukh@gmail.com>
2020-03-29 13:43:17 +0200

    WIP

--- Kurs/obliczenia_10.py
+++ Kurs/obliczenia_10.py
@@ -2,14 +2,14 @@ number_1 = int(input("Podaj pierwszą liczbę: "))
 number_2 = int(input("Podaj drugą liczbę: "))
 operation = input("Podaj rodzaj operacji: ")
 if operation == "-" :
-    print(number_1 - number_2)
+    print("Wynik: ", number_1 - number_2)
 elif operation == "+" :
-    print(number_1 + number_2)
+    print("Wynik: ", number_1 + number_2)
 elif operation == "*" :
-    print(number_1 * number_2)
+    print("Wynik: ", number_1 * number_2)
 elif operation == "/" :
     if number_2 != 0 :
-        print(round(number_1 / number_2, 2))
+        print("Wynik: ", round(number_1 / number_2, 2))
     else:
         print("Nie można dzielić przez zero.")
 else:
---
+++ Kurs/temperatura_13.py
@@ -0,0 +1,10 @@
+t_1 = float(input("Podaj temperaturę z pierwszego dnia: "))
+t_2 = float(input("Podaj temperaturę z drugiego dnia: "))
+t_3 = float(input("Podaj temperaturę z trzeciego dnia: "))
+t_4 = float(input("Podaj temperaturę z czwartego dnia: "))
+t_5 = float(input("Podaj temperaturę z piątego dnia: "))
+t_6 = float(input("Podaj temperaturę z szostego dnia: "))
+t_7 = float(input("Podaj temperaturę z siodmego dnia: "))
+
+print("Średnia z temperatór to: ", int((t_1 + t_2 + t_3 + t_4 + t_5 + t_6 + t_7)/7), "stopni. ")
+
