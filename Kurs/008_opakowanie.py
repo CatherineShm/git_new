22b27318abe35f1617bba78949fbb216ca9677b7
CatherineShm <katerynashmaliukh@gmail.com>
2020-03-29 11:30:14 +0200

    we'

--- Kurs/008_opakowanie.py
+++ Kurs/008_opakowanie.py
@@ -1,2 +1,6 @@
-wysokosc = float(input("Podaj wysokość opakowania: "))
-szerokosc = float(input("Podaj szerokośćopakowania: "))
+x = int(input("Podaj wysokość opakowania: "))
+y = int(input("Podaj szerokośćopakowania: "))
+z = int(input("Podaj szerokośćopakowania: "))
+
+print("Czy objętość większa od 1 litra?" , x * y * z > 1000)
+print("Objętość : ", str(x * y * z))
